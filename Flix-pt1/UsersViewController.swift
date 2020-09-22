3a7f41a66c1941849290f34c783371baceff6487
purvi15t <pgupta1598@gmail.com>
2020-09-22 16:23:39 -0500

    Added app icon in home screen and styled launch screen along with scrollable table view.

---
+++ Flix-pt1/UsersViewController.swift
@@ -0,0 +1,54 @@
+//
+//  UsersViewController.swift
+//  Flix-pt1
+//
+//  Created by purvi gupta on 9/21/20.
+//  Copyright © 2020 Purvi. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+
+    @IBOutlet weak var UserTableView: UITableView!
+    
+    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
+            "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
+            "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
+            "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
+            "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UserTableView.dataSource=self
+        UserTableView.delegate=self
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        cell.textLabel!.text = data[indexPath.row]
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

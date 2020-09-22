d11b4ef3255c76d817bb6a9663e350c44dbbc688
purvi15t <pgupta1598@gmail.com>
2020-09-21 21:44:22 -0500

    Initial Commit

---
+++ Flix-pt1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix-pt1
+//
+//  Created by purvi gupta on 9/21/20.
+//  Copyright © 2020 Purvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
